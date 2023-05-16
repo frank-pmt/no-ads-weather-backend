18abcb8e63a576827251bd6d925c373dd3c89b28
frank-pmt <133765061+frank-pmt@users.noreply.github.com>
2023-05-16 13:00:52 +0100

    Fixed test

--- client/src/__tests__/App.test.tsx
+++ client/src/__tests__/App.test.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from '../App';
 
 test('renders learn react link', () => {
   render(<App />);
+  const element = screen.getByText(/Weather App/i);
+  expect(element).toBeInTheDocument();
 });
