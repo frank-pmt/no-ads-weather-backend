812534a0d3b7ccc4ca6dce10b164fd8d8b1d786c
frank-pmt <133765061+frank-pmt@users.noreply.github.com>
2023-05-16 11:48:16 +0100

    Added Server

---
+++ server/src/app.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import path from 'path';
+import {WeatherApi} from './weather';
+import dotenv from 'dotenv';
+
+class App {
+
+  private _app: express.Application;
+  private _weatherApi: WeatherApi;
+
+
+  constructor() {
+    this._app = express();
+    this._weatherApi = new WeatherApi();
+    this.initApp();
+  }
+
+  private initApp(): void {
+    dotenv.config();
+    this._app.use(express.static(path.join(__dirname, '../../client/build')));
+    this._app.use('/api/weather', this._weatherApi.getRouter());
+  }
+
+  public start() {
+    const port = process.env.PORT || 3000;
+    this._app.listen(port, () => {
+      console.log(`App listening on the port ${port}`);
+    });
+  }
+
+}
+
+export default App;
+
+
+
+
+
