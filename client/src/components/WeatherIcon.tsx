4c360aa2020f9f8ea065a2e40624c548e5f012fd
frank-pmt <133765061+frank-pmt@users.noreply.github.com>
2023-05-16 19:18:55 +0100

    Added missing mapping

--- client/src/components/WeatherIcon.tsx
+++ client/src/components/WeatherIcon.tsx
@@ -12,6 +12,7 @@ function WeatherIcon({ type, size }: WeatherIconProps) {
           'cloudy': <img className="icon" src={"images/cloudy-" + size + ".png"} alt="Cloudy" />,
           'drizzly': <img className="icon" src={"images/drizzle-" + size + ".png"} alt="Drizzle" />,
           'rainy': <img className="icon" src={"images/rain-" + size + ".png"} alt="Rainy" />,
+          'foggy': <img className="icon" src={"images/fog-" + size + ".png"} alt="Foggy" />,
           'overcast': <img className="icon" src={"images/overcast-" + size + ".png"} alt="Overcast" />,
           'thunderstorm': <img className="icon" src={"images/thunderstorm-" + size + ".png"} alt="Thunderstorm" />
         }[type]
